bee90c53ecbaf4b0c39179ab92e891d190d12a16
Hossein <77438070+Hossein-Haghshenas@users.noreply.github.com>
2022-08-02 23:02:04 +0430

    Create GetStartBtn component

---
+++ src/components/GetStartBtn.jsx
@@ -0,0 +1,9 @@
+const GetStartBtn = () => {
+  return (
+    <button className="bg-[#00df9a] transition delay-100 ease-in-out hover:bg-[#2dffbc] w-[200px] rounded-md font-medium my-6 mx-auto py-3 text-[#292929]">
+      Get Started
+    </button>
+  );
+};
+
+export default GetStartBtn;
